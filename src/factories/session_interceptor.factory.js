38928b49350d5c0a1f3c0d4b2be59e205e0bce73
Andrew Clinton <aclinton@anecka.com>
2016-06-29 13:53:56 -0400

    getting closer

--- src/factories/session_interceptor.factory.js
+++ src/factories/session_interceptor.factory.js
@@ -20,8 +20,7 @@
         /* --- PUBLIC METHODS --- */
 
         function _responseError(response) {
-            console.log(response.config.config);
-            var config = response.config || {};
+            var config = response.config.config || {};
             // Token has expired or some other Auth error
             if (!config.ignoreAuth && response.status == 401 && error_count < max_error_count) {
                 error_count++;
