0920d1eb0a1ffa9ca88b3242dd8f694bbb359578
Andrew Clinton <aclinton@anecka.com>
2016-06-29 11:13:53 -0400

    the interceptor is not working

--- src/factories/session_interceptor.factory.js
+++ src/factories/session_interceptor.factory.js
@@ -8,6 +8,7 @@
     Factory.$inject = ['$injector', '$q'];
 
     function Factory($injector, $q) {
+        console.log('this is the interceptor factory...');
         var factory = {
             responseError: _responseError
         };
@@ -17,24 +18,25 @@
         /* --- PUBLIC METHODS --- */
 
         function _responseError(response) {
-        		// Token has expired or some other Auth error
-                if (response.status == 401) {
-                	console.log('Had a 401 error');
-                    var AuthService = $injector.get('RRAuthFactory');
-                    var $http = $injector.get('$http');
-                    var deferred = $q.defer();
+            console.log(response);
+    		// Token has expired or some other Auth error
+            if (response.status == 401) {
+            	console.log('Had a 401 error');
+                var AuthService = $injector.get('RRAuthFactory');
+                var $http = $injector.get('$http');
+                var deferred = $q.defer();
 
-                    // Create a new session (recover the session)
-                    // We use login method that logs the user in using the current credentials and
-                    // returns a promise
-                    AuthService.getToken().then(deferred.resolve, deferred.reject);
+                // Create a new session (recover the session)
+                // We use login method that logs the user in using the current credentials and
+                // returns a promise
+                AuthService.getToken().then(deferred.resolve, deferred.reject);
 
-                    // When the session recovered, make the same backend call again and chain the request
-                    return deferred.promise.then(function() {
-                        return $http(response.config);
-                    });
-                }
-                return $q.reject(response);
+                // When the session recovered, make the same backend call again and chain the request
+                return deferred.promise.then(function() {
+                    return $http(response.config);
+                });
+            }
+            return $q.reject(response);
         }
     }
 })();
