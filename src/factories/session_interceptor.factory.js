58e0d5cd5b1392ffb30d3668771be9eb7381a6cf
Andrew Clinton <aclinton@anecka.com>
2016-06-29 11:22:48 -0400

    prevent infinite recursion

--- src/factories/session_interceptor.factory.js
+++ src/factories/session_interceptor.factory.js
@@ -9,6 +9,9 @@
 
     function Factory($injector, $q) {
         console.log('this is the interceptor factory...');
+        var error_count = 0;
+        var max_error_count = 5;
+
         var factory = {
             responseError: _responseError
         };
@@ -18,10 +21,8 @@
         /* --- PUBLIC METHODS --- */
 
         function _responseError(response) {
-            console.log(response);
-    		// Token has expired or some other Auth error
+            // Token has expired or some other Auth error
             if (response.status == 401) {
-            	console.log('Had a 401 error');
                 var AuthService = $injector.get('RRAuthFactory');
                 var $http = $injector.get('$http');
                 var deferred = $q.defer();
@@ -31,10 +32,18 @@
                 // returns a promise
                 AuthService.getToken().then(deferred.resolve, deferred.reject);
 
-                // When the session recovered, make the same backend call again and chain the request
-                return deferred.promise.then(function() {
-                    return $http(response.config);
-                });
+                if (error_count >= max_error_count) {
+                    error_count = 0;
+                    return deferred.promise;
+                } else {
+                    // When the session recovered, make the same backend call again and chain the request
+                    return deferred.promise.then(function() {
+                        error_count = 0;
+                        return $http(response.config);
+                    }, function() {
+                        error_count++;
+                    });
+                }
             }
             return $q.reject(response);
         }
