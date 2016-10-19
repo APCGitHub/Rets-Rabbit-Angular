14be8212f40a8690ef12b091ec510b8174b0db8d
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-19 12:22:11 -0400

    new v1 property factory

--- src/factories/index.js
+++ src/factories/index.js
@@ -6,6 +6,7 @@
 			'rets-rabbit-angular.factory.auth',
 			'rets-rabbit-angular.factory.metadata',
 			'rets-rabbit-angular.factory.queries',
+			'rets-rabbit-angular.factory.properties.v1',
 			'rets-rabbit-angular.factory.properties.v2',
 			'rets-rabbit-angular.factory.session.interceptor'
 		]);
---
+++ src/factories/v1.properties.factory.js
@@ -0,0 +1,53 @@
+/**
+ * Created by aclinton on 10/19/16.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('rets-rabbit-angular.factory.properties.v1', [])
+        .factory('PropertyFactoryV1', Factory);
+
+    Factory.$inject = ['$q', '$http', 'ApiConfig', 'KeyStorageService'];
+
+    function Factory ($q, $http, ApiConfig, KeyStorageService) {
+        var factory = {
+            search: _search,
+            metadata: _metadata
+        };
+
+        return factory;
+
+        function _search(server_hash, query_string) {
+            var deferred = $q.defer();
+
+            $http.get(ApiConfig.v1.apiUrl + server_hash + '/listing/search?' + query_string, {
+                headers: {
+                    Authorization: 'Bearer ' + KeyStorageService.v1.getToken()
+                }
+            }).success(function (res){
+                deferred.resolve(res);
+            }).error(function (res){
+                deferred.reject(res);
+            });
+
+            return deferred.promise;
+        }
+
+        function _metadata(server_hash) {
+            var deferred = $q.defer();
+
+            $http.get(ApiConfig.v1.apiUrl + server_hash + '/metadata/explorer', {
+                headers: {
+                    Authorization: 'Bearer ' + KeyStorageService.v1.getToken()
+                }
+            }).success(function (res){
+                deferred.resolve(res);
+            }).error(function (res) {
+                deferred.reject(res);
+            });
+
+            return deferred.promise;
+        }
+    }
+})();
